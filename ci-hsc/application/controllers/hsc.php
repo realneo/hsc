f691c98881b716667ceb5cb02ec40bd133823288
Fahad Kassim <fahad@yoteyote.com>
2014-07-09 14:39:40 +0300

    Home is coming up nicely

--- ci-hsc/application/controllers/hsc.php
+++ ci-hsc/application/controllers/hsc.php
@@ -1,11 +1,25 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Hsc extends CI_Controller {
+    //great for initializing default variables
+    protected  $data = array();
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('usuals');
+
+    }
+
 
 
 	public function index()
 	{
-		$this->load->view('includes/header');
+        $this->session->set_userdata("branch_id",7);
+
+		$data['manual_invoice']=$this->usuals->getTotalManualInvoices(0);
+
+        $this->load->view('includes/header',$data);
+
 		$this->load->view('default_content');
 		$this->load->view('includes/footer');
 	}
