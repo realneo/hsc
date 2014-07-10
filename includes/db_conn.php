8e0f4a8264553f697aedb586634f31c1d17bfff0
Fahad Kassim <fahad@yoteyote.com>
2014-07-10 10:55:35 +0300

    Ignored db_conn :fallen_leaf:

--- includes/db_conn.php
+++ includes/db_conn.php
@@ -1,7 +1,7 @@
 <?php
 	// Connect to the database
 
-    $db = new mysqli('localhost', 'root', 'root', 'hsc_db');
+    $db = new mysqli('localhost', 'root', 'admin', 'hsc_db');
     
     if($db->connect_errno){
         die('Failed to Connect to the Database');
