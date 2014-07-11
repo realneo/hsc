b98ac07859148d4a3592f6f0b50b25209daa4278
Fahad Kassim <fahad@yoteyote.com>
2014-07-11 17:55:04 +0300

    Pagination COmpleted :hamburger:

--- ci-hsc/application/views/notifications/notify.php
+++ ci-hsc/application/views/notifications/notify.php
@@ -1,11 +1,11 @@
-<div class="row">
+<div class="" style="margin-top: 5px;">
     <?php foreach($notifications as $not){?>
         <div class="row ">
-<div class="well row">
+<div class="well row well-noti" >
 
         <div class="col-lg-1">
             <p class="lead"><span style="font-size: 0.49em;"><?php echo custom_date_format($not['date']);?></span><br>
-                <?php echo $not['timeonly'];?></p>
+                <?php echo make_me_bold($not['timeonly']);?></p>
             </div>
             <div class="col-lg-9"><p><?php echo $not['log'];?></p></div>
 
