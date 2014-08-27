0dab30f4c6a7fd948e24fbe2b7e72250d4b15ef0
Fahad Kassim <fahad@yoteyote.com>
2014-08-27 14:57:36 +0300

    Started responsiveness

--- ci-hsc/application/controllers/staff.php
+++ ci-hsc/application/controllers/staff.php
@@ -36,9 +36,13 @@ class Staff extends CI_Controller{
 
         $this->data['logs']=$this->usuals->get_recent_activities();
         $this->data['branches']=$this->usuals->get_branches();
+
+        $this->data['branches']=$this->usuals->get_branches();
+
         $this->data['users']=$this->vouchers->get_users();
         $this->data['auth_type']=$this->staffs->get_auth_type();
         $this->data['gender']=$this->staffs->gender_type();
+        $this->data['staffs']= $this->staffs->get_branch_users();
 
     }
 
--- ci-hsc/application/views/staff/change_branch.php
+++ ci-hsc/application/views/staff/change_branch.php
@@ -1,7 +1,93 @@
 <?php
-/**
- * Created Using Fadsel Engine.
- * User: Fahad
- * Date: 8/27/14
- * Time: 1:53 PM
- */
+$online = check_if_online();
+?>
+
+<div class="row">
+    <?php
+    //col-md-offset-2 col-md-8 col-lg-offset-3
+    foreach($staffs as $key=>$staff){?>
+        <!--                        mpya-->
+        <div class="col-lg-12">
+
+            <div class="well well-list">
+                <div class="col-lg-1 visible-lg visible-md">
+                    <img src="<?php
+                    switch($this->staffs->get_profile($staff['id'])[0]['gender']){
+                        case 'male':
+                            //echo "http://www.gravatar.com/avatar/".md5( strtolower( trim(  $staff['email'] ) ) )."?s=200&d=mm&";//d=".base_url('assets/img/default-user-icon-profile.png');
+                            if($this->staffs->get_profile($staff['id'])[0]['img_url'] AND $online == true){
+                                echo $this->staffs->get_profile($staff['id'])[0]['img_url'];
+                                //echo "http://www.gravatar.com/avatar/".md5( strtolower( trim(  $staff['email'] ) ) )."?s=200";
+                            }else{
+                                echo base_url('assets/img/default-user-icon-profile.png');
+                            }
+                            break;
+                        case 'female':
+
+                            if($this->staffs->get_profile($staff['id'])[0]['img_url']  AND $online==true){
+                                echo $this->staffs->get_profile($staff['id'])[0]['img_url'];
+                                //echo "http://www.gravatar.com/avatar/".md5( strtolower( trim(  $staff['email'] ) ) )."?s=200";
+                            }else{
+                                echo base_url('assets/img/default-user-icon-profile-pink.png');
+                            }
+
+                            // echo "http://www.gravatar.com/avatar/".md5( strtolower( trim(  $staff['email'] ) ) )."?s=200&d=retro";//;&d=".base_url('assets/img/default-user-icon-profile-pink.png');
+                            break;
+
+                        default : echo base_url('assets/img/default-user-icon-profile.png'); break;
+                    }?>
+                                " alt="" class="img-circle img-responsive">
+
+
+                </div>
+                <div class="col-lg-5 well-list-description">
+                    <h2><?php echo $this->staffs->get_profile($staff['id'])[0]['first_name']." "
+                            .$this->staffs->get_profile($staff['id'])[0]['last_name'];?></h2>
+                    <p>
+                        <?php
+                        echo make_me_bold($this->staffs->get_auth_type_name($staff['auth_type']));
+                        if($staff['branch_id']!=0){
+                            echo " currently assigned at ".make_me_bold($this->usuals->get_branch_name($staff['branch_id']));
+                        }
+
+
+                        ?></p>
+                    <p class="small text-muted">
+                        <?php echo $staff['email'];?>
+                    </p>
+                </div>
+                <div class="col-lg-5">
+                    <div class="text-right">
+                        <div class="responsive-select form-group col-lg-12 col-lg-8 col-xs-8 col-sm-8 text-right pull-right">
+                            <form
+                                action="<?php echo base_url('staff/change_staff_details') . '/' . $staff['id'] ?>">
+                                <div class="input-group ">
+                                    <select class='form-control input-append' name='auth_type'>
+                                        <?php
+
+                                        foreach($this->usuals->get_all_branches() as $branch){?>
+                                            <option class="reset-fahad" <?php
+                                                if($branch['id']==$staff['branch_id']){echo "selected";}
+                                                ?>> <?php echo $branch['name'];echo ($branch['id']<=7)?" Branch":"";?></option>
+
+                                        <?php } ?>
+
+                                    </select>
+              <span class="input-group-btn">
+                  <input value="update" type="submit" class="btn btn-cool input-append"/>
+              </span>
+                                </div>
+
+
+                                </form>
+
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+
+        </div>
+    <?php } ?>
+</div>
